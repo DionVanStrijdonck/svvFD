0ec444d6ca1529b9db1ca4cf33b16953f08c8fd5
Foulds28 <deacon.foulds@outlook.com>
2017-03-08 11:32:49 +0100

    Deacon - Added main python file

---
+++ main.py
@@ -0,0 +1,21 @@
+# SVV Flight Dynamics Group 17 - 03 - 2017 Flight 3
+# Main Python File
+# Authors:  Deacon Foulds,
+
+
+# Packages
+
+
+import numpy as np
+from scipy import linalg as lg
+from matplotlib import pyplot as plt
+import control as ctl
+
+
+
+
+
+
+
+
+
